39d543223c0d4d539f26edd48bab6fa10be9cd62
Muhannad <muhanned9980@gmail.com>
2024-10-23 11:10:02 +0300

    finshing the you do (make a new route for the listing the aray empty

--- controllers/listings.js
+++ controllers/listings.js
@@ -5,4 +5,16 @@ const router = express.Router();
 
 const Listing = require('../models/listing');
 
+router.get('/', async (req, res) => {
+    try{
+        const listings =  await Listing.find();
+        console.log(listings)
+        res.send('Listings index page');
+    }
+    catch(error){
+        console.log(error);
+    }
+    
+  });
+
 module.exports = router;
