6ca7c776db147faacb56f2245b1c007de52e923f
Muhannad <muhanned9980@gmail.com>
2024-10-23 13:50:48 +0300

    adding the form of the new listing

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,22 +1,29 @@
 // controllers/listings.js
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const Listing = require('../models/listing');
+const Listing = require("../models/listing");
 
-router.get('/', async (req, res) => {
-    try{
-        const listings =  await Listing.find();
-        console.log(listings)
-        // res.send('Listings index page');
-        res.render('listings/index.ejs')
-    }
-    catch(error){
-        console.log(error);
-    }
-    
-  });
+router.get("/", async (req, res) => {
+  try {
+    const listings = await Listing.find();
+    console.log(listings);
+    // res.send('Listings index page');
+    res.render("listings/index.ejs");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+router.get('/new' , (req, res) =>{
+res.render("listings/new.ejs");
+});
+
+router.post('/', async (req , res)=>{
+  req.body.owner= req.session.user._id;
+  await Listing.create(req.body);
+  res.redirect('/listings');  
+})
 
 module.exports = router;
