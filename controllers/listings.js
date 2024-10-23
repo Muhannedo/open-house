429ff9d8ff0cf422aad0418ed8195f9322a3b6c8
Muhannad <muhanned9980@gmail.com>
2024-10-23 14:40:32 +0300

    listing the listings

--- controllers/listings.js
+++ controllers/listings.js
@@ -7,12 +7,15 @@ const Listing = require("../models/listing");
 
 router.get("/", async (req, res) => {
   try {
-    const listings = await Listing.find();
-    console.log(listings);
+    // const listings = await Listing.find();
+    const populatedListings = await Listing.find({}).populate('owner');
+    // console.log(listings);
+    console.log('Populated Listings:', populatedListings);
     // res.send('Listings index page');
-    res.render("listings/index.ejs");
+    res.render("listings/index.ejs",{listings: populatedListings});
   } catch (error) {
     console.log(error);
+    res.redirect('/');
   }
 });
 
@@ -26,4 +29,19 @@ router.post('/', async (req , res)=>{
   res.redirect('/listings');  
 })
 
+
+router.get('/:listingId', async (req, res) => {
+  try {
+    const populatedListings = await Listing.findById(
+      req.params.listingId
+    ).populate('owner');
+
+    res.render('listings/show.ejs', {
+      listing: populatedListings,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
 module.exports = router;
